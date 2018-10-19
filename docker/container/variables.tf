fb5c1cf133b906cd9beeed2e5ac8013bd859e2be
Igor Batkanov <batkanov@gmail.com>
2018-10-19 08:37:03 -0400

    Terraform: example for docker containers deployment

---
+++ docker/container/variables.tf
@@ -0,0 +1,15 @@
+variable "image" {
+  description = "Image for container"
+}
+
+variable "name" {
+  description = "Name for container"
+}
+
+variable "int_port" {
+  description = "Internal port"
+}
+
+variable "ext_port"  {
+  description = "External Port"
+}
